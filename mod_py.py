1017fdee8f8d82fa93ff536678c1577ad67acaf9
lubkz <letreiroomega@gmail.com>
2024-06-09 02:04:15 -0300

    Add files via upload

--- mod_py.py
+++ mod_py.py
@@ -1,5 +1,5 @@
-# Dicionário FAQ com palavras-chave e respostas
-faq = {
+# Dicionário FAQ com palavras-chave e respostas.
+faq_respostas = {
     "1" : "O horário de atendimento é...",
     "2" : "O local de atendimento é na...",
     "3" : "O endereço da rua é...",
@@ -9,13 +9,27 @@ faq = {
 
 }
 
+#Dicionário Faq das opções do usuário.
+faq_perguntas = {
+    "1" : "Qual o horário de atendimento",
+    "2" : "Quais os locais em que a empresa trabalha",
+    "3" : "Qual o endereço da empresa em Fortaleza",
+    "4" : "Qual os serviços da empresa",
+    "5" : "Quais são as regiões que a empresa atende",
+    "6" : "Qual número posso usar para entrar em contato"
+
+}
+
 def encontrar_resposta(pergunta):
-        for palavra_chave, resposta in faq.items():
+        for palavra_chave, resposta in faq_respostas.items():
             if palavra_chave in pergunta:
                 return resposta
 
 
 while True:
+    print("Digite o número correspondente a sua dúvida!")
+    for palavra_chave, resposta in faq_perguntas.items():
+        print(palavra_chave, resposta)
     pergunta = input("Digite sua pergunta: ")
     resposta = encontrar_resposta(pergunta)
     print(resposta)
