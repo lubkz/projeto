dda13f032a4e8b5ebb988e0dda99f4056e8e53d5
lubkz <letreiroomega@gmail.com>
2024-06-10 23:18:31 -0300

    Add files via upload

--- mod_py.py
+++ mod_py.py
@@ -1,57 +1,77 @@
+import random
 # Dicionário FAQ com palavras-chave e respostas.
 # Na esquerda trmos palavras chaves e na direita temos as respostas relacionadas a cada palavra chave.
 # Os dois valores estão ligados por conta do ":" e são atribuídos a variáveis mais pra frente.
 faq_respostas = {
-    "1" : "O horário de atendimento é das 9:00 até às 17:00 horas!",
-    "2" : "O local de atendimento é na parte leste da empresa ao lado da biblioteca!",
-    "3" : "A empresa atualmente reside na Rua Leopoldina Assunção, 1257, Centro, Fortaleza!",
-    "4" : "A empresa EnterPrizes é especializada no atendimento e no conforto dos clientes! Nossa empresa dará o melhor para tornar seu ambiente o mais agradável possível para seus clientes!",
-    "5" : "No momento, a EnterPrizes atende apenas nas regiões de Fortaleza, Maracanaú, Sobral e Itapipoca!",
-    "6" : "Claro! Caso deseje um atendimento humano, use o número +55 (85) 9 9732-2742"
+    "1": "O horário de atendimento é das 9:00 até às 17:00 horas!",
+    "2": "O local de atendimento é na parte leste da empresa ao lado da biblioteca!",
+    "3": "A empresa atualmente reside na Rua Leopoldina Assunção, 1257, Centro, Fortaleza!",
+    "4": "A empresa EnterPrizes é especializada no atendimento e no conforto dos clientes! Nossa empresa dará o melhor para tornar seu ambiente o mais agradável possível para seus clientes!",
+    "5": "No momento, a EnterPrizes atende apenas nas regiões de Fortaleza, Maracanaú, Sobral e Itapipoca!",
+    "6": "Claro! Caso deseje um atendimento humano, use o número +55 (85) 9 9732-2742"
 
 }
 
-#Dicionário Faq das opções do usuário.
-#Na esquerda trmos palavras chaves e na direita temos os valores relacionadas a cada palavra chave.
-#Os dois valores estão ligados por conta do ":" e são atribuídos a variáveis mais pra frente.
+# Dicionário Faq das opções do usuário.
+# Na esquerda trmos palavras chaves e na direita temos os valores relacionadas a cada palavra chave.
+# Os dois valores estão ligados por conta do ":" e são atribuídos a variáveis mais pra frente.
 
 faq_perguntas = {
-    "1" : "Qual o horário de atendimento?",
-    "2" : "Quais os locais em que a empresa trabalha?",
-    "3" : "Qual o endereço da empresa EnterPrizes?",
-    "4" : "Quais os serviços da empresa?",
-    "5" : "Quais são as regiões que a empresa atende?",
-    "6" : "Qual número posso usar para entrar em contato?"
+    "1": "Qual o horário de atendimento?",
+    "2": "Quais os locais em que a empresa trabalha?",
+    "3": "Qual o endereço da empresa EnterPrizes?",
+    "4": "Quais os serviços da empresa?",
+    "5": "Quais são as regiões que a empresa atende?",
+    "6": "Qual número posso usar para entrar em contato?"
 
 }
+
 
 # Função principal que vai encontrar a resposta.
 def encontrar_resposta(pergunta):
-        #Para cada palavra_chave e resposta nos itens da faq_respostas:
-        for palavra_chave, resposta in faq_respostas.items():
-            #Se houver uma palavra_chave dentro da pergunta:
-            if palavra_chave == pergunta:
-                #Retorna o valor correspondente da resposta no faq_RESPOSTAS.
-                #Exemplo: Digitei o número "1". Ele vai reconhecer o "1" e responder o que vem depois do ":" na linha do "1" no dicionário faq_RESPOSTAS.
-                return resposta
-        # Caso nao haja nenhuma palavra_chave dentro da pergunta, o looping termina e naõ retorna nenhum valor.
-        # Sobrando apenas executar o valor padrão abaixo.
-        # Retorna um valor de string padrão com a mensagem a seguir:   
-        return "Desculpe, não entendi qual sua dúvida."
+    # Para cada palavra_chave e resposta nos itens da faq_respostas:
+    for palavra_chave, resposta in faq_respostas.items():
+        # Se houver uma palavra_chave dentro da pergunta:
+        if palavra_chave == pergunta:
+            # Retorna o valor correspondente da resposta no faq_RESPOSTAS.
+            # Exemplo: Digitei o número "1". Ele vai reconhecer o "1" e responder o que vem depois do ":" na linha do "1" no dicionário faq_RESPOSTAS.
+            return resposta
+    # Caso nao haja nenhuma palavra_chave dentro da pergunta, o looping termina e naõ retorna nenhum valor.
+    #Sobrando apenas executar o valor padrão abaixo.
+    # Retorna um valor de string padrão com a mensagem a seguir:
+    return "Desculpe, não entendi qual sua dúvida."
 
-#Executa indefinidamente dentro do programa, até see interrompido
+#Apenas uma saudação aleatória a cada novo início de loop.
+def saudacao_inicial():
+    saudacoes = [
+        "Olá! Como posso ajudar hoje?",
+        "Bem-vindo! Em que posso assisti-lo?",
+        "Oi! Precisa de alguma informação?",
+        "Olá! Seja bem-vindo!"
+    ]
+    #retorna um valor aleatório dentro do dicionario para a variável
+    return random.choice(saudacoes)
+
+# Executa indefinidamente dentro do programa, até ser interrompido
 while True:
-    # Mensagem de boas-vindas:
-    print("Digite o número correspondente a sua dúvida!")
+    # Apenas printando espaços vazios no prompt para organização.
+    print("")
+    # Mensagem de boas-vindas aleatória:
+    print(saudacao_inicial())
+    print("")
+    #Mensagem de comando padrão:
+    print("Digite o número correspondente à sua dúvida:")
+    print("")
 
     # Serve apenas para printar as opções de perguntas e seus respectivos números.
     # Para cada palavra_chave, resposta nos itens da faq_PERGUNTAS:
     for palavra_chave, resposta in faq_perguntas.items():
-        #Exibe todos os valores do dicionário de perguntas:
+        # Exibe todos os valores do dicionário de perguntas:
         print(f"{palavra_chave}: {resposta}")
-        
+
     # Fazendo a pergunta ao usuário e atribuindo a informação dele a uma variável "pergunta", utilizada na função encontrar_resposta.
     # Aqui é o início de todo o código.
+    print("")
     pergunta = input("Digite sua pergunta: ")
 
     # Aqui eu chamo a função principal, que é responsável por dar um valor a minha variável resposta.
@@ -59,9 +79,11 @@ while True:
     resposta = encontrar_resposta(pergunta)
 
     # Aqui faco aparecer a resposta.
+    print("")
     print(resposta)
 
     # Função extra para saber se a pessoa deseja fazer outra pergunta.
+    print("")
     confirm = input("Deseja fazer mais alguma pergunta? S/N: ")
 
     # Tornando a resposta em letras maiúsculas para facilitar o entendimento da máquina.
@@ -70,3 +92,8 @@ while True:
     # Se a resposta for "N" ele dá um "break" no looping e o código acaba.
     if confirm == "N":
         break
+    else:
+        #Apenas printando espaços vazios no prompt.
+        print("")
+        print("")
+
